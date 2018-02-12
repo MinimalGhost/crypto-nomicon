09cb43516e8ba57b2aededf52f5d0dbb8d39c9f1
nspp99b <chromepanthers@gmail.com>
2018-02-12 17:54:56 -0500

    made crypto container rendering conditional

--- cryptonomicon-react/src/App.js
+++ cryptonomicon-react/src/App.js
@@ -67,7 +67,7 @@ class App extends Component {
             return <Signup history={routerProps.history} setUser={this.setLoggedInUser} />
           }} />
       </Switch>
-        <CryptoContainer />
+        { this.state.auth.currentUser != null && <CryptoContainer /> }
       </div>
     );
   }
