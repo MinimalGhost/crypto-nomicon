9ec9e59086a3f2abf8f972cfcba53917c2b29389
nspp99b <chromepanthers@gmail.com>
2018-02-13 22:04:10 -0500

    added conditional update in case of bad api data

--- cryptonomicon-api/app/controllers/api/v1/cryptos_controller.rb
+++ cryptonomicon-api/app/controllers/api/v1/cryptos_controller.rb
@@ -11,8 +11,10 @@ class Api::V1::CryptosController < ApplicationController
       response = RestClient.get("https://api.coinmarketcap.com/v1/ticker/")
       iKnowKungFu = JSON.parse(response)
       iKnowKungFu.each do |obj|
-        c = Crypto.find_by(symbol: obj['symbol'])
-        c.update(name: obj['name'], symbol: obj['symbol'], rank: obj['rank'], price_usd: obj['price_usd'], price_btc: obj['price_btc'], daily_volume_usd: obj['daily_volume_usd'], market_cap_usd: obj['market_cap_usd'], available_supply: obj['available_supply'], total_supply: obj['total_supply'], percent_change_1h: obj['percent_change_1h'], percent_change_24h: obj['percent_change_24h'], percent_change_7d: obj['percent_change_7d'], last_updated: obj['last_updated'])
+        c = Crypto.find_by(name: obj['name'])
+        if c
+          c.update(name: obj['name'], symbol: obj['symbol'], rank: obj['rank'], price_usd: obj['price_usd'], price_btc: obj['price_btc'], daily_volume_usd: obj['daily_volume_usd'], market_cap_usd: obj['market_cap_usd'], available_supply: obj['available_supply'], total_supply: obj['total_supply'], percent_change_1h: obj['percent_change_1h'], percent_change_24h: obj['percent_change_24h'], percent_change_7d: obj['percent_change_7d'], last_updated: obj['last_updated'])
+        end
       end
     # end
     # @user = current_user
