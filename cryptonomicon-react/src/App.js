19dc1b73225bbd178cacc70a5e9b54148e2c36c6
nspp99b <chromepanthers@gmail.com>
2018-05-08 14:46:29 -0400

    adds comments and styling concerns

--- cryptonomicon-react/src/App.js
+++ cryptonomicon-react/src/App.js
@@ -12,6 +12,21 @@ class App extends Component {
     auth: { currentUser: null }
   };
 
+  componentDidMount() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      adapter.auth.getLoggedInUser().then(user => {
+        if (user) {
+          this.setState({ auth: { currentUser: user } });
+        } else {
+          this.setState({ auth: { currentUser: null } });
+        };
+      });
+    } else {
+      console.log('No token found');
+    };
+  };
+
   setLoggedInUser = (user) => {
     localStorage.setItem('token', user.token);
     this.setState({
@@ -25,8 +40,7 @@ class App extends Component {
   };
 
   removeLoggedInUser = () => {
-    adapter.auth.logout(this.state.auth.currentUser)
-    .then(() => {
+    adapter.auth.logout(this.state.auth.currentUser).then(() => {
       localStorage.removeItem('token');
       this.setState({
         auth: { currentUser: null }
@@ -35,37 +49,23 @@ class App extends Component {
     });
   };
 
-  componentDidMount() {
-    const token = localStorage.getItem('token');
-    if (token) {
-      adapter.auth.getLoggedInUser().then(user => {
-        if (user) {
-          this.setState({ auth: {currentUser: user } });
-        } else {
-          this.setState({ auth: { currentUser: null } });
-        };
-      });
-    } else {
-      console.log('No token found');
-    };
-  };
+
+
+  //render navbar; render login or signup based on route
+  //conditionally render cryptocontainer on auth
 
   render() {
     return (
       <div>
-        <Navbar
-          currentUser={this.state.auth.currentUser}
-          logOut={this.removeLoggedInUser}
-        />
-      <Switch>
-        <Route exact path='/login' render={(routerProps) => {
-            return <Login history={routerProps.history} setUser={this.setLoggedInUser} />
+        <Navbar currentUser={this.state.auth.currentUser} logOut={this.removeLoggedInUser} />
+        <Switch>
+          <Route exact path='/login' render={(routerProps) => {
+              return <Login history={routerProps.history} setUser={this.setLoggedInUser} />
           }} />
-
-        <Route exact path='/signup' render={(routerProps) => {
-            return <Signup history={routerProps.history} setUser={this.setLoggedInUser} />
+          <Route exact path='/signup' render={(routerProps) => {
+              return <Signup history={routerProps.history} setUser={this.setLoggedInUser} />
           }} />
-      </Switch>
+        </Switch>
         { this.state.auth.currentUser != null && <CryptoContainer /> }
       </div>
     );
--- cryptonomicon-react/src/components/Login.js
+++ cryptonomicon-react/src/components/Login.js
@@ -1,30 +1,36 @@
 import React from 'react';
-import adapter from '../adapter'
-import { Link } from 'react-router-dom'
+import adapter from '../adapter';
+import { Link } from 'react-router-dom';
+
+//controlled component to handle login
 
 class Login extends React.Component {
+
   state = {
     username: '',
     password: ''
-  }
+  };
 
+  //call login fetch using state as args, on success
+  //change browser URL and setUser token for hot login
   handleLogin = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     adapter.auth.login(this.state.username, this.state.password).then(res => {
       if (res.error) {
-        alert(res.error)
+        alert(res.error);
       } else {
-        this.props.history.push("/")
-        this.props.setUser(res)
-      }
-    })
-  }
+        this.props.history.push("/");
+        this.props.setUser(res);
+      };
+    });
+  };
 
+  //control text input fields
   onInputChange = (e) => {
     this.setState({
       [e.target.name]: e.target.value
-    })
-  }
+    });
+  };
 
   render() {
     return (
@@ -32,14 +38,16 @@ class Login extends React.Component {
         <form onSubmit={this.handleLogin}>
           <label>Username: </label> <br />
           <input value={this.state.username} type="text" name="username" onChange={this.onInputChange} /> <br />
+
           <label>Password: </label> <br />
           <input value={this.state.password} type="password" name="password" onChange={this.onInputChange} /> <br />
-          <input className="submit-button" type="submit" />
+
+          <input className="submit-button" type="submit" value="Login"/>
         </form>
         <Link to='/signup'>No Account? Sign Up</Link>
       </div>
-    )
-  }
-}
+    );
+  };
+};
 
-export default Login
+export default Login;
